ceac252c9dcb53ba51d38c8036ecf9d1699006ad
TaewonBae <gerrard1283@naver.com>
2023-01-04 21:12:41 +0900

    Initial Commit

---
+++ AfreecaTV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AfreecaTV
+//
+//  Created by 배태원 on 2023/01/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
